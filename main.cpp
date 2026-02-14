7edc41eb274372a4fd882fbb92b301c18751fb6b
TakutoOhkawa <ohkawa.takuto@gmail.com>
2026-02-14 21:44:13 +0900

    ハクション関数を追記しました。

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+void hakushon();
+
 int main()
 {
 	std::cout << "Hello, World!" << std::endl;
+	hakushon();
 	return 0;
+}
+
+void hakushon()
+{
+	std::cout << "anpanman!" << std::endl;
 }
